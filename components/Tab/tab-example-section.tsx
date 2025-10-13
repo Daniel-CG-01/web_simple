33212a8cc0970c7e29f1ea1580667fc1c70e027f
DanielZR05 <danielzanon2005@gmail.com>
2025-10-13 14:44:31 +0200

    Acabar última ampliación

--- components/Tab/tab-example-section.tsx
+++ components/Tab/tab-example-section.tsx
@@ -1,17 +1,23 @@
 'use client';
 import React from 'react';
+import { useState } from 'react';
 import TabButton from './tab-button';
 import TabExample from './tab_example';
 import { EXAMPLES } from '@/scripts/data';
 
 export default function TabExampleSection() {
+  const [selected, setSelected] = useState<keyof typeof EXAMPLES | null>(null); // Estado para la pestaña seleccionada
   function handleClick() {
-
   }
   return (
     <>
     <section id="tab_section" className='my-12 md:mx-12 mx-4 flex flex-col'>
-      <TabButton isSelected={true} onClick={handleClick}>{EXAMPLES.state.title}</TabButton>
+      <div className='flex justify-start space-x-2'>
+        <TabButton isSelected={true} onClick={handleClick}>{EXAMPLES.components.title}</TabButton>
+        <TabButton isSelected={false} onClick={handleClick}>{EXAMPLES.jsx.title}</TabButton>
+        <TabButton isSelected={false} onClick={handleClick}>{EXAMPLES.props.title}</TabButton>
+        <TabButton isSelected={false} onClick={handleClick}>{EXAMPLES.state.title}</TabButton>
+      </div>
       <TabExample/>
     </section>
     </>
