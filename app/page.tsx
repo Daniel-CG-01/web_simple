0d2857baacb8b8b52ebc432544626f256403f0cb
DanielZR05 <danielzanon2005@gmail.com>
2025-10-13 13:35:47 +0200

    Mejora de la página

--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,13 @@ import jsxImg from "@/public/jsx-ui.png";
 import propsImg from '@/public/config.png';
 import stateImg from "@/public/state-mgmt.png";
 import CoreConcepts from "@/components/core-concepts";
+import CoreConceptsList from "@/components/core-concepts-list";
 
 export default function Home() {
   return (
   <>
   <Header/>
-  <section id="conceptos" className="m-12 flex flex-col justify-center bg-violet-800 min-w-44">
-    <h1 className="text-2xl font-extrabold text-slate-100 p-3 mx-auto mb-6 overflow-clip">
-      Conceptos básicos de React
-    </h1>
-    <ul className="m-3 md:m-8 flex flex-col md:flex-row space-y-16 md:space-y-0 md:space-x-6">
-      <CoreConcepts 
-        title="Componentes"
-        description="El núcleo de React son los componentes. Un componente es una pieza reutilizable de código que representa una parte de la interfaz de usuario. Los componentes pueden ser tan simples como un botón o tan complejos como una página completa."
-        image={componentsImg}/>
-      <CoreConcepts 
-        title="Componentes"
-        description="El núcleo de React son los componentes. Un componente es una pieza reutilizable de código que representa una parte de la interfaz de usuario. Los componentes pueden ser tan simples como un botón o tan complejos como una página completa."
-        image={jsxImg}/>
-      <CoreConcepts 
-        title="Componentes"
-        description="El núcleo de React son los componentes. Un componente es una pieza reutilizable de código que representa una parte de la interfaz de usuario. Los componentes pueden ser tan simples como un botón o tan complejos como una página completa."
-        image={propsImg}/>
-      <CoreConcepts 
-        title="Componentes"
-        description="El núcleo de React son los componentes. Un componente es una pieza reutilizable de código que representa una parte de la interfaz de usuario. Los componentes pueden ser tan simples como un botón o tan complejos como una página completa."
-        image={stateImg}/>
-    </ul>
-  </section>
+  <CoreConceptsList/>
   </>
   );
 }
